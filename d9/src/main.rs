56c8fa0ba7db9934091d5127c1757efc08acf970
Ruairidh Williamson <williamsonruairidh@gmail.com>
2023-12-09 09:05:26 +0000

    Day 9

---
+++ d9/src/main.rs
@@ -0,0 +1,75 @@
+use std::io::stdin;
+
+fn main() {
+    let total: isize = stdin()
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let sequence = parse_sequence(&line);
+            prev_element(&sequence)
+        })
+        .sum();
+    println!("{total}");
+}
+
+fn parse_sequence(sequence: &str) -> Vec<isize> {
+    sequence.split(' ').map(|x| x.parse().unwrap()).collect()
+}
+
+fn next_element(sequence: &[isize]) -> isize {
+    // println!("{sequence:?}");
+    if all_zero(sequence) {
+        return 0;
+    }
+    let next_diff = next_element(&differences(&sequence));
+    let next = sequence.last().unwrap() + next_diff;
+    // println!("{sequence:?} {next}");
+    next
+}
+
+fn prev_element(sequence: &[isize]) -> isize {
+    if all_zero(sequence) {
+        return 0;
+    }
+    let prev_diff = prev_element(&differences(&sequence));
+    let prev = sequence.first().unwrap() - prev_diff;
+    prev
+}
+
+fn differences(sequence: &[isize]) -> Vec<isize> {
+    assert!(sequence.len() > 1);
+    sequence
+        .iter()
+        .enumerate()
+        .skip(1)
+        .map(|(i, x)| (sequence[i - 1], x))
+        .map(|(a, b)| b - a)
+        .collect()
+}
+
+fn all_zero(sequence: &[isize]) -> bool {
+    sequence.iter().all(|x| *x == 0)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{next_element, prev_element};
+
+    #[test]
+    fn all_zeros() {
+        let e = next_element(&[0, 0, 0, 0, 0]);
+        assert_eq!(e, 0);
+    }
+
+    #[test]
+    fn linear() {
+        let e = next_element(&[0, 1, 2, 3, 4]);
+        assert_eq!(e, 5);
+    }
+
+    #[test]
+    fn linear_prev() {
+        let e = prev_element(&[2, 3, 4]);
+        assert_eq!(e, 1);
+    }
+}
