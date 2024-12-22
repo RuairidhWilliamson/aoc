b6cd75e3127fa6d7cf63146c0679248497dfe729
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-12-22 16:35:03 +0000

    Optimise day 22

--- src/day22.rs
+++ src/day22.rs
@@ -1,13 +1,12 @@
-use std::collections::{HashMap, HashSet};
-
 pub fn solve_part1(input: &str) -> usize {
     parse_initials(input).map(|s| calc_n(s, 2000)).sum()
 }
 
 pub fn solve_part2(input: &str) -> u32 {
-    let mut seqs = HashMap::new();
-    parse_initials(input).for_each(|s| seq_map(s, 2001, &mut seqs));
-    seqs.into_values().max().unwrap()
+    let mut seqs = SequenceMap::new(0);
+    let mut already_sold = SequenceMap::new(false);
+    parse_initials(input).for_each(|s| seq_map(s, 2001, &mut seqs, &mut already_sold));
+    *seqs.d.iter().max().unwrap()
 }
 
 fn parse_initials(input: &str) -> impl Iterator<Item = usize> + use<'_> {
@@ -37,15 +36,74 @@ fn prices(mut s: usize, n: usize) -> impl Iterator<Item = i8> {
     })
 }
 
+fn prices_collect(s: usize, n: usize) -> Vec<i8> {
+    prices(s, n).collect()
+}
+
 type Sequence = [i8; 4];
 
-fn seq_map(s: usize, n: usize, seqs: &mut HashMap<Sequence, u32>) {
-    let mut already_sold = HashSet::<Sequence>::new();
-    let prices: Vec<i8> = prices(s, n).collect();
+#[repr(transparent)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct SequenceHash(u32);
+
+impl SequenceHash {
+    const MAX: SequenceHash = Self::new([9, 9, 9, 9]);
+
+    const fn new(seq: Sequence) -> Self {
+        Self(
+            ((seq[0] + 10) as u32) << 15
+                | ((seq[1] + 10) as u32) << 10
+                | ((seq[2] + 10) as u32) << 5
+                | ((seq[3] + 10) as u32),
+        )
+    }
+}
+
+struct SequenceMap<T> {
+    d: Box<[T]>,
+}
+
+impl<T: Copy> SequenceMap<T> {
+    fn new(default: T) -> Self {
+        Self {
+            d: (0..SequenceHash::MAX.0).map(|_| default).collect(),
+        }
+    }
+
+    fn set_all(&mut self, value: T) {
+        self.d.fill(value);
+    }
+
+    #[allow(unused)]
+    fn get(&self, s: SequenceHash) -> &T {
+        self.d.get(s.0 as usize).unwrap()
+    }
+
+    fn get_mut(&mut self, s: SequenceHash) -> &mut T {
+        self.d.get_mut(s.0 as usize).unwrap()
+    }
+}
+
+impl SequenceMap<bool> {
+    fn insert(&mut self, s: SequenceHash) -> bool {
+        let b = self.get_mut(s);
+        if *b {
+            false
+        } else {
+            *b = true;
+            true
+        }
+    }
+}
+
+fn seq_map(s: usize, n: usize, seqs: &mut SequenceMap<u32>, already_sold: &mut SequenceMap<bool>) {
+    already_sold.set_all(false);
+
+    let prices: Vec<i8> = prices_collect(s, n);
     for w in prices.windows(5) {
-        let sequence = [0, 1, 2, 3].map(|i| w[i + 1] - w[i]);
+        let sequence = SequenceHash::new([0, 1, 2, 3].map(|i| w[i + 1] - w[i]));
         if already_sold.insert(sequence) {
-            *seqs.entry(sequence).or_default() += w[4] as u32;
+            *seqs.get_mut(sequence) += w[4] as u32;
         }
     }
 }
@@ -72,10 +130,11 @@ fn prices_test() {
         prices(123, 10).collect::<Vec<_>>(),
         vec![3, 0, 6, 5, 4, 4, 6, 4, 4, 2]
     );
-    let mut seqs = HashMap::new();
-    seq_map(123, 10, &mut seqs);
-    assert_eq!(*seqs.get(&[-1, -1, 0, 2]).unwrap(), 6);
-    assert_eq!(*seqs.get(&[-3, 6, -1, -1]).unwrap(), 4);
+    let mut seqs = SequenceMap::new(0);
+    let mut already_sold = SequenceMap::new(false);
+    seq_map(123, 10, &mut seqs, &mut already_sold);
+    assert_eq!(*seqs.get(SequenceHash::new([-1, -1, 0, 2])), 6);
+    assert_eq!(*seqs.get(SequenceHash::new([-3, 6, -1, -1])), 4);
 }
 
 #[test]
