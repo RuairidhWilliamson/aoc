2f12823dd8ea1874f0bc052c038ad8b8bf13d922
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-12-25 11:29:33 +0000

    Day 24

--- src/day24.rs
+++ src/day24.rs
@@ -8,167 +8,36 @@ pub fn solve_part1(input: &str) -> usize {
 
 pub fn solve_part2(input: &str) -> String {
     let mut p = Puzzle::parse(input);
-    let mut gate_map: HashMap<&str, Gate> = p.gates.iter().map(|g| (g.output, *g)).collect();
-    let mut swaps = Vec::new();
+    let mut gc = GateCorrector::new(&p);
     let mut i = 0;
     loop {
-        let target = format!("z{i:02}");
-        let Some(g) = gate_map.get(&target.as_str()) else {
+        let Some(_) = gc.get_z_gate(i) else {
             break;
         };
-        let target = format!("z{:02}", i + 1);
-        let Some(_) = gate_map.get(&target.as_str()) else {
-            break;
-        };
-        let target_num = format!("{i:02}");
-
-        if g.gate_kind != GateKind::Xor {
-            let other_g = gate_map
-                .values()
-                .find(|g| {
-                    g.input1[1..] == target_num
-                        && g.input2[1..] == target_num
-                        && g.gate_kind == GateKind::Xor
-                })
-                .unwrap();
-            let other_g_dependent = gate_map
-                .values()
-                .find(|g| {
-                    (g.input1 == other_g.output || g.input2 == other_g.output)
-                        && g.gate_kind == GateKind::Xor
-                })
-                .unwrap();
-            println!("{g:?} {other_g_dependent:?}");
-            add_swap(*g, *other_g_dependent, &mut swaps, &mut gate_map);
+        // Don't run on final carry bit
+        if gc.get_z_gate(i + 1).is_some() && gc.check_z_gate(i).is_none() {
             i = 0;
             continue;
         }
-        if i <= 1 {
-            i += 1;
-            continue;
-        }
-
-        if let Some(d1) = gate_map.get(g.input1) {
-            if let Some(d2) = gate_map.get(g.input2) {
-                println!("d1 = {d1:?}    d2 = {d2:?}");
-                match (d1.gate_kind, d2.gate_kind) {
-                    (GateKind::Xor, GateKind::Or) => {
-                        if d1.input1[1..] != target_num || d1.input2[1..] != target_num {
-                            todo!()
-                        }
-                        let a = gate_map.get(d2.input1).unwrap();
-                        let b = gate_map.get(d2.input2).unwrap();
-                        if a.gate_kind != GateKind::And {
-                            todo!()
-                        }
-                        if b.gate_kind != GateKind::And {
-                            todo!()
-                        }
-                        if a.input1[1..] == target_num {
-                            if a.input2[1..] != target_num {
-                                todo!()
-                            }
-                        }
-                        if a.input1[1..] == target_num {
-                            if a.input2[1..] != target_num {
-                                todo!()
-                            }
-                        }
-                        println!("a = {a:?}    b = {b:?}");
-                    }
-                    (GateKind::Or, GateKind::Xor) => {
-                        if d2.input1[1..] != target_num || d2.input2[1..] != target_num {
-                            todo!()
-                        }
-                        let a = gate_map.get(d1.input1).unwrap();
-                        let b = gate_map.get(d1.input2).unwrap();
-                        if a.gate_kind != GateKind::And {
-                            todo!()
-                        }
-                        if b.gate_kind != GateKind::And {
-                            todo!()
-                        }
-                        if a.input1[1..] == target_num {
-                            if a.input2[1..] != target_num {
-                                todo!()
-                            }
-                        }
-                        if a.input1[1..] == target_num {
-                            if a.input2[1..] != target_num {
-                                todo!()
-                            }
-                        }
-                        println!("a = {a:?}    b = {b:?}");
-                    }
-                    (GateKind::Xor, GateKind::Xor) => {
-                        let swap_target =
-                            if d2.input1[1..] == target_num && d2.input2[1..] == target_num {
-                                d1
-                            } else {
-                                d2
-                            };
-                        let prev_target_num = format!("{:02}", i - 1);
-                        let a = gate_map
-                            .values()
-                            .find(|g| {
-                                g.input1[1..] == prev_target_num
-                                    && g.input2[1..] == prev_target_num
-                                    && g.gate_kind == GateKind::And
-                            })
-                            .unwrap();
-                        let b = gate_map
-                            .values()
-                            .find(|g| {
-                                g.gate_kind == GateKind::Or
-                                    && (g.input1 == a.output || g.input2 == a.output)
-                            })
-                            .unwrap();
-                        println!("{swap_target:?} {b:?}");
-                        add_swap(*swap_target, *b, &mut swaps, &mut gate_map);
-                        i = 0;
-                        continue;
-                    }
-                    (GateKind::Or, GateKind::And) => {
-                        let a = gate_map
-                            .values()
-                            .find(|g| {
-                                g.input1[1..] == target_num
-                                    && g.input2[1..] == target_num
-                                    && g.gate_kind == GateKind::Xor
-                            })
-                            .unwrap();
-                        println!("{d2:?} {a:?}");
-                        add_swap(*d2, *a, &mut swaps, &mut gate_map);
-                        i = 0;
-                        continue;
-                    }
-                    _ => {
-                        println!("{g:?}");
-                        println!("\t{d1:?}");
-                        println!("\t{d2:?}");
-                        todo!()
-                    }
-                }
-            }
-        }
         i += 1;
     }
-    // let mut i = 0;
-    // loop {
-    //     let target = format!("z{i:02}");
-    //     let Some(g) = gate_map.get(&target.as_str()) else {
-    //         break;
-    //     };
-
-    //     print_tree(&gate_map, g.output, 5);
-    //     println!();
-
-    //     i += 1;
-    // }
-    println!("{swaps:?}");
-    for (s1, s2) in &swaps {
-        p.gates.iter_mut().find(|g| g.output == *s1).unwrap().output = *s2;
-        p.gates.iter_mut().find(|g| g.output == *s2).unwrap().output = *s1;
+    for (s1, s2) in &gc.swaps {
+        let i = p
+            .gates
+            .iter()
+            .enumerate()
+            .find(|(_, g)| g.output == *s1)
+            .unwrap()
+            .0;
+        let j = p
+            .gates
+            .iter()
+            .enumerate()
+            .find(|(_, g)| g.output == *s2)
+            .unwrap()
+            .0;
+        p.gates[i].output = *s2;
+        p.gates[j].output = *s1;
     }
     p.run();
     let x = p.calc('x');
@@ -176,34 +45,159 @@ pub fn solve_part2(input: &str) -> String {
     let z = p.calc('z');
     let expected = x + y;
     assert_eq!(expected, z, "\n{expected:b}\n{z:b}");
-    let mut swaps: Vec<&str> = swaps.into_iter().flat_map(|(s1, s2)| [s1, s2]).collect();
+    let mut swaps: Vec<&str> = gc.swaps.into_iter().flat_map(|(s1, s2)| [s1, s2]).collect();
     swaps.sort_unstable();
     swaps.join(",")
 }
 
-fn add_swap<'a>(
-    mut a: Gate<'a>,
-    mut b: Gate<'a>,
-    swaps: &mut Vec<(&'a str, &'a str)>,
-    gate_map: &mut HashMap<&'a str, Gate<'a>>,
-) {
-    swaps.push((a.output, b.output));
-    std::mem::swap(&mut a.output, &mut b.output);
-    gate_map.insert(a.output, a);
-    gate_map.insert(b.output, b);
+struct GateCorrector<'a> {
+    gate_map: HashMap<&'a str, Gate<'a>>,
+    swaps: Vec<(&'a str, &'a str)>,
 }
 
-fn print_tree<'a>(gate_map: &HashMap<&'a str, Gate<'a>>, out: &'a str, depth: usize) {
-    if depth == 0 {
-        return;
+impl<'a> GateCorrector<'a> {
+    fn new(puzzle: &Puzzle<'a>) -> Self {
+        let gate_map = puzzle.gates.iter().map(|g| (g.output, *g)).collect();
+        let swaps = Vec::new();
+        Self { gate_map, swaps }
     }
-    let Some(g) = gate_map.get(out) else {
-        return;
-    };
-    println!("{g:?}");
-    print_tree(gate_map, g.input1, depth - 1);
-    print_tree(gate_map, g.input2, depth - 1);
+
+    fn get_z_gate(&self, num: usize) -> Option<Gate<'a>> {
+        let target = format!("z{num:02}");
+        self.gate_map.get(&target.as_str()).copied()
+    }
+
+    fn check_z_gate(&mut self, i: usize) -> Option<NoSwap> {
+        let gate = self.get_z_gate(i).unwrap();
+        if gate.kind != GateKind::Xor {
+            let child_replacement = self
+                .gate_map
+                .values()
+                .find(|g| {
+                    g.input1[1..] == gate.output[1..]
+                        && g.input2[1..] == gate.output[1..]
+                        && g.kind == GateKind::Xor
+                })
+                .unwrap();
+            let replacement: Vec<_> = self
+                .gate_map
+                .values()
+                .filter(|g| {
+                    (g.input1 == child_replacement.output || g.input2 == child_replacement.output)
+                        && g.kind == GateKind::Xor
+                })
+                .collect();
+            assert_eq!(replacement.len(), 1);
+            self.add_swap(*replacement[0], gate)?;
+        }
+        let gate = self.get_z_gate(i).unwrap();
+        let Some(child1) = self.gate_map.get(gate.input1).copied() else {
+            return Some(NoSwap);
+        };
+        let Some(child2) = self.gate_map.get(gate.input2).copied() else {
+            return Some(NoSwap);
+        };
+        if child1.input1.starts_with('x')
+            && child1.input2.starts_with('y')
+            && child1.kind == GateKind::Xor
+        {
+            self.check_input_xor(child1, &gate.output[1..]);
+            self.check_carry(child2, i);
+        } else {
+            self.check_input_xor(child2, &gate.output[1..]);
+            self.check_carry(child1, i);
+        }
+        Some(NoSwap)
+    }
+
+    fn check_input_xor(&mut self, gate: Gate<'a>, expected_num: &str) -> Option<NoSwap> {
+        if gate.input1[1..] == *expected_num
+            && gate.input2[1..] == *expected_num
+            && gate.kind == GateKind::Xor
+        {
+            // Good
+            return Some(NoSwap);
+        }
+        let replacement = self
+            .gate_map
+            .values()
+            .find(|g| {
+                g.input1[1..] == *expected_num
+                    && g.input2[1..] == *expected_num
+                    && g.kind == GateKind::Xor
+            })
+            .unwrap();
+        self.add_swap(gate, *replacement)
+    }
+
+    fn check_carry(&mut self, gate: Gate<'a>, i: usize) -> Option<NoSwap> {
+        if i == 1 {
+            return Some(NoSwap);
+        }
+        if gate.kind != GateKind::Or {
+            todo!();
+        }
+        let child1 = *self.gate_map.get(gate.input1).unwrap();
+        let child2 = *self.gate_map.get(gate.input2).unwrap();
+        if child1.kind != GateKind::And || child2.kind != GateKind::And {
+            todo!()
+        }
+        let (prev, other) = if child1.input1.starts_with('x') && child1.input2.starts_with('y') {
+            (child1, child2)
+        } else {
+            (child2, child1)
+        };
+        let num = format!("{:02}", i - 1);
+        if prev.input1[1..] != num {
+            todo!()
+        }
+        if prev.input2[1..] != num {
+            todo!()
+        }
+        self.check_carry_chain(other, i - 1)
+    }
+
+    fn check_carry_chain(&mut self, gate: Gate<'a>, i: usize) -> Option<NoSwap> {
+        if i == 1 {
+            return Some(NoSwap);
+        }
+        let child1 = *self.gate_map.get(gate.input1).unwrap();
+        let child2 = *self.gate_map.get(gate.input2).unwrap();
+        let (term, _other) = if child1.input1.starts_with('x') && child1.input2.starts_with('y') {
+            (child1, child2)
+        } else {
+            (child2, child1)
+        };
+        if term.kind != GateKind::Xor {
+            todo!()
+        }
+        Some(NoSwap)
+    }
+
+    #[allow(dead_code)]
+    fn print_tree(&self, out: &'a str, depth: usize) {
+        if depth == 0 {
+            return;
+        }
+        let Some(g) = self.gate_map.get(out) else {
+            return;
+        };
+        println!("{g:?}");
+        self.print_tree(g.input1, depth - 1);
+        self.print_tree(g.input2, depth - 1);
+    }
+
+    #[must_use]
+    fn add_swap(&mut self, mut a: Gate<'a>, mut b: Gate<'a>) -> Option<NoSwap> {
+        self.swaps.push((a.output, b.output));
+        std::mem::swap(&mut a.output, &mut b.output);
+        self.gate_map.insert(a.output, a);
+        self.gate_map.insert(b.output, b);
+        None
+    }
 }
+
+struct NoSwap;
 
 struct Puzzle<'a> {
     states: HashMap<&'a str, bool>,
@@ -240,7 +234,7 @@ impl<'a> Puzzle<'a> {
                 Gate {
                     input1: input1.min(input2),
                     input2: input1.max(input2),
-                    gate_kind,
+                    kind: gate_kind,
                     output,
                 }
             })
@@ -255,7 +249,7 @@ impl<'a> Puzzle<'a> {
                 let g = &self.gates[i];
                 if let Some(v1) = self.states.get(g.input1) {
                     if let Some(v2) = self.states.get(g.input2) {
-                        self.states.insert(g.output, g.gate_kind.calc(*v1, *v2));
+                        self.states.insert(g.output, g.kind.calc(*v1, *v2));
                         self.gates.swap_remove(i);
                         continue;
                     }
@@ -281,7 +275,7 @@ impl<'a> Puzzle<'a> {
 struct Gate<'a> {
     input1: &'a str,
     input2: &'a str,
-    gate_kind: GateKind,
+    kind: GateKind,
     output: &'a str,
 }
 
