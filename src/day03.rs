205a2e2af2353d7214183de007bffabf98d16dd4
Ruairidh Williamson <williamsonruairidh@gmail.com>
2025-12-04 23:13:38 +0000

    Minor tweak

--- src/day03.rs
+++ src/day03.rs
@@ -15,14 +15,14 @@ fn inner<const N: usize>(input: &str) -> u64 {
         let mut nums = &line[..];
         let mut joltage = 0;
         for i in 0..N {
-            let (a_index, a_byte) = nums[..nums.len() - (N - i - 1)]
+            let (a_index, &a_byte) = nums[..nums.len() - (N - i - 1)]
                 .as_bytes()
                 .into_iter()
                 .enumerate()
                 .rev()
                 .max_by_key(|(_, x)| *x)
                 .unwrap();
-            let a = (*a_byte as char).to_digit(10).unwrap();
+            let a = (a_byte as char).to_digit(10).unwrap();
             joltage = joltage * 10 + a as u64;
             nums = &nums[a_index + 1..];
         }
