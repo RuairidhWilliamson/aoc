4195d8529b7a33de02733bcb65c866737070ad18
Ruairidh Williamson <williamsonruairidh@gmail.com>
2025-12-06 14:56:26 +0000

    Optimise parse_usize

--- src/day06.rs
+++ src/day06.rs
@@ -68,12 +68,14 @@ pub fn part2(input: &str) -> usize {
     grand_total
 }
 
-fn parse_usize_iter(iter: impl Iterator<Item = u8>) -> Option<usize> {
-    let numb: Vec<u8> = iter.collect();
-    let numb_str = str::from_utf8(&numb).unwrap().trim();
-    if numb_str.is_empty() {
-        return None;
+fn parse_usize_iter(mut iter: impl Iterator<Item = u8>) -> Option<usize> {
+    let mut out: usize = 0;
+    let mut found_digit = false;
+    while let Some(digit) = iter.next() {
+        if digit.is_ascii_digit() {
+            found_digit = true;
+            out = out * 10 + (digit - b'0') as usize;
+        }
     }
-    let numb: usize = numb_str.parse().unwrap();
-    Some(numb)
+    if found_digit { Some(out) } else { None }
 }
