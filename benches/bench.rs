9d099e1fbfc20ebb500b357744d20121363a7705
Ruairidh Williamson <williamsonruairidh@gmail.com>
2025-12-15 09:12:51 +0000

    More benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -90,6 +90,66 @@ fn day07_part2(input: String) {
     aoc::day07::part2(&input);
 }
 
+#[library_benchmark]
+#[benches::first(args = ["test_input/day08.txt", "input/day08.txt"], setup = read_input)]
+fn day08_part1(input: String) {
+    aoc::day08::part1(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day08.txt", "input/day08.txt"], setup = read_input)]
+fn day08_part2(input: String) {
+    aoc::day08::part2(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day09.txt", "input/day09.txt"], setup = read_input)]
+fn day09_part1(input: String) {
+    aoc::day09::part1(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day09.txt", "input/day09.txt"], setup = read_input)]
+fn day09_part2(input: String) {
+    aoc::day09::part2(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day10.txt", "input/day10.txt"], setup = read_input)]
+fn day10_part1(input: String) {
+    aoc::day10::part1(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day10.txt", "input/day10.txt"], setup = read_input)]
+fn day10_part2(input: String) {
+    aoc::day10::part2(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day11.txt", "input/day11.txt"], setup = read_input)]
+fn day11_part1(input: String) {
+    aoc::day11::part1(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day11.txt", "input/day11.txt"], setup = read_input)]
+fn day11_part2(input: String) {
+    aoc::day11::part2(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day12.txt", "input/day12.txt"], setup = read_input)]
+fn day12_part1(input: String) {
+    aoc::day12::part1(&input);
+}
+
+#[library_benchmark]
+#[benches::first(args = ["test_input/day12.txt", "input/day12.txt"], setup = read_input)]
+fn day12_part2(input: String) {
+    aoc::day12::part2(&input);
+}
+
 library_benchmark_group!(
     name = days;
     benchmarks =
@@ -100,6 +160,11 @@ library_benchmark_group!(
         day05_part1, day05_part2,
         day06_part1, day06_part2,
         day07_part1, day07_part2,
+        day08_part1, day08_part2,
+        day09_part1, day09_part2,
+        day10_part1, day10_part2,
+        day11_part1, day11_part2,
+        day12_part1, day12_part2,
 );
 
 main!(library_benchmark_groups = days);
